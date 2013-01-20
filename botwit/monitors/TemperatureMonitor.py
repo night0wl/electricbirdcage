495cb18d5f4aaee946a033e025b98436762a71e9
Matt Revell <matthew.revell@lonelyplanet.com>
2013-01-20 07:19:15 +0000

    Make monitors a module and impl. pipe for heating

--- botwit/monitors/TemperatureMonitor.py
+++ botwit/monitors/TemperatureMonitor.py
@@ -1,9 +1,9 @@
 import temper
+from multiprocessing import Process, Pipe
 from time import sleep, time
 
 MIN = 60
 HOUR = 60*60
-DAY = 60*60*24
 
 class TemperatureMonitor(object):
     def __init__(self):
@@ -12,7 +12,6 @@ class TemperatureMonitor(object):
         self.events = [
                 (time(), self.dev.get_temperature())
                 ]
-#        self.pipe = pipe
 
     def get_diffs(self, current_temperature):
         get_events = lambda x : [
@@ -21,31 +20,45 @@ class TemperatureMonitor(object):
 
         get_diff = lambda x : -min(x)[1]--current_temperature
 
-        print get_events(MIN), get_events(HOUR), get_events(DAY)
+        #print get_events(MIN), get_events(HOUR)
 
         min_events = get_events(MIN)
         hour_events = get_events(HOUR)
-        day_events = get_events(DAY)
 
         min_diff = min_events and get_diff(min_events) or 0.0
         hour_diff = hour_events and get_diff(hour_events) or 0.0
-        day_diff = day_events and get_diff(day_events) or 0.0
 
-        return min_diff, hour_diff, day_diff
+        return min_diff, hour_diff
 
 
-    def run(self):
+    def run(self, pipe):
         while True:
             current_temperature = self.dev.get_temperature()
+            if pipe.poll():
+                msg = pipe.recv()
+                if msg == "get_diffs":
+                    pipe.send(self.get_diffs(current_temperature))
+                elif msg == "halt":
+                    break
+
             if self.events[0][1] != current_temperature:
                 try:
                     self.events.insert(0, (time(), current_temperature))
                 except usb.USBError:
                     print "USB Error"
                     continue
-            print self.get_diffs(current_temperature)
+            #print self.get_diffs(current_temperature)
             sleep(1)
+
+        pipe.send("quitting")
 
 if __name__ == "__main__":
     mon = TemperatureMonitor()
-    mon.run()
+    parent, child = Pipe()
+    p = Process(target=mon.run, args=(child,))
+    p.start()
+    parent.send("get_diffs")
+    print parent.recv()
+    parent.send("halt")
+    print parent.recv()
+    p.join()
