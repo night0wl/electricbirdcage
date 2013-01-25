a0f33d22b2ed72d270e6972eb49e25a5a843b1cd
Matt Revell <matthew.revell@lonelyplanet.com>
2013-01-25 00:10:29 +0000

    Ref simple_agent.py and comments

--- simple_agent.py
+++ simple_agent.py
@@ -1,52 +1,49 @@
 #!/usr/bin/env python
 
-DB_FILE = "/home/perseus/twitter_bots/stream_bot/stream_bot.db"
+#   A simple botwit agent
+#
+#   Author: Matt Revell
 
+import redis
 import sys
-import sqlite3 as lite
 
-from bots import StreamBot
-from responders import SimpleResponse
+from botwit.bots import StreamBot
+from botwit.responders import SimpleResponse
 
-def sql_get_one(db, sql, args):
-    return sql_exec(db, sql, args, num=1)
+def get_creds(botname):
+    """
+    Get Twitter API credetials from redis
 
-def sql_exec(db, sql, args, num=0, commit=False, first_column_only=True):
-    cur = db.cursor()
-    cur.execute(sql, args)
-
-    if commit:
-        self.db.commit()
-        return True
-    elif num == 1:
-        return cur.fetchone()
-    else:
-        result = cur.fetchall()
-        if first_column_only:
-            return [x[0] for x in result]
-        else:
-            return [x for x in result]
-
-def get_creds(db, botname):
-    string = ''.join([
-        "SELECT consumer_key, consumer_secret, access_token,",
-        "access_token_secret FROM accounts WHERE account = ?"
-        ])
-
-    return sql_get_one(db, string, [botname])
+    Returns (
+        consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret
+        )
+    """
+    base_key = "%s:" % botname.lower()
+    red = redis.StrictRedis()
+    return (
+        red.get(base_key + "consumer_key"),
+        red.get(base_key + "consumer_secret"),
+        red.get(base_key + "access_token"),
+        red.get(base_key + "access_token_secret")
+        )
 
 def main():
-    replies = {
-        "^.*achoo.*$": "Bless You"
-        }
-    responders = [
-        SimpleResponse(replies)
-        ]
+    """
+    Run the agent
+    """
+    simple_replies = {
+            "^.*achoo.*$": "Bless You"
+            }
 
     try:
         botname = sys.argv[1]
-        db = lite.connect(DB_FILE)
-        creds = get_creds(db, botname)
+        responders = [
+            SimpleResponse(botname, simple_replies)
+            ]
+        creds = get_creds(botname)
         bot = StreamBot(botname, creds, responders)
         bot.listen(None, ['@' + botname])
     except KeyboardInterrupt:
